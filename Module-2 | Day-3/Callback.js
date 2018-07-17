cbfc49d709c7a142effab87659a494100ed41197
Syed <sazeem@grepruby.com>
2018-07-17 16:50:18 +0530

    Requested Changes Implemented.

--- Module-2 | Day-3/Callback.js
+++ Module-2 | Day-3/Callback.js
@@ -1,58 +1,16 @@
-var fs = require('fs');
+const fs = require('fs');
 
-
-fs.readFile('file1.txt', function(err, data) {
-	
-	fs.appendFile('file2.txt', data, function (err) {
-	
+fs.readFile('file1.txt', (err, data) => {	
+	fs.appendFile('file2.txt', data,  (err) => {
 		if (err) throw err;
-		
-		console.log('Content of file1 appended to file2!');
-
-		fs.readFile('file2.txt', function(err, data) {
-	
-			fs.writeFile('file3.txt', data, function (err) {
-				
+    else
+		  console.log('Content of file1 appended to file2!');
+		fs.readFile('file2.txt', (err, data) => {
+			fs.writeFile('file3.txt', data,  (err) => {				
 				if (err) throw err;
-				console.log('Content of file2 saved to file3!');
-
+        else
+				  console.log('Content of file2 saved to file3!');
 			});  
-	
-		});
-		
-	});
-	
-});
-
-
-
-
-
-/*
-	fs.open('mynewfile2.txt', 'w', function (err, file) {
-  		if (err) throw err;
-  		console.log('Saved!');
+		});		
 	});	
-
-	
-
-	fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
-  		if (err) throw err;
-  		console.log('Updated!');
-	});
-
-	fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
-	  	if (err) throw err;
-  		console.log('Replaced!');
-	});
-
-	fs.unlink('mynewfile2.txt', function (err) {
-  		if (err) throw err;
-  		console.log('File deleted!');
-	});
-
-	fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-  		if (err) throw err;
-  		console.log('File Renamed!');
-	});
-*/
+});
--- Module-2 | Day-3/Promise.js
+++ Module-2 | Day-3/Promise.js
@@ -1,28 +1,16 @@
-var fs = require('fs');
+const = require('fs');
 
-var readFile1 = function(){
-		
+const readFile1 = function(){		
 	return new Promise((success,error) =>{
-
-    fs.readFile('file1.txt', function(err, data) {
-      if(err) 
-        return error(err);
-      else
-        return success(data);
-
-    });    
+    fs.readFile('file1.txt', (err,data) => err ? error(err) : success(data));    
   });	
 };
 
-var appendFile = function(data){
-
-  return new Promise((success,error) => {
-  
-    fs.appendFile('file2.txt',data, function (err) {     
-      
+const appendFile = function(data){
+  return new Promise((success,error) => {  
+    fs.appendFile('file2.txt',data, function (err) {           
       if (err)
-        return error(err);
-      
+        return error(err);      
       else {
         console.log('Content of file1 appended to file2!'); 
         return success();
@@ -30,46 +18,38 @@ var appendFile = function(data){
     });
   });
 }; 
-var readFile2 = function(){
-    
+const readFile2 = function(){  
   return new Promise((success,error) => {
-
     fs.readFile('file2.txt', function(err, data) {
       if(err) 
         return error(err);
       else{
-        console.log("File2 read!")
+        console.log("File2 read!");
         success(data);
       }
     });    
   }); 
 };
 
-var writeFile = function(data){
-
-  return new Promise((success,error) => {
-  
-    fs.writeFile('file3.txt', data, function (err) {        
-      
+const writeFile = function(data){
+  return new Promise((success,error) => {  
+    fs.writeFile('file3.txt', data, function (err) {              
       if (err)
         return error(err);
-
       else{            
         console.log('Content of file2 saved to file3!');
         return success();
       }
-
     });
-    });
+  });
 };
 
-var errorCallback = function(err){
+const errorCallback = function(err){
   console.log(err);
 };
 
-
 readFile1()
-.then(data => appendFile(data))
-.then(readFile2)
-.then(result => writeFile(result))
-.catch(errorCallback);
+ .then(data => appendFile(data))
+ .then(readFile2)
+ .then(result => writeFile(result))
+ .catch(errorCallback);
