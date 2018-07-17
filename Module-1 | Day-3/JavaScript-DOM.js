29486008abeb38dbde44d9aff1dc4aa45c6fe362
Syed <sazeem@grepruby.com>
2018-07-17 12:24:54 +0530

    Added HTML file.

---
+++ Module-1 | Day-3/JavaScript-DOM.js
@@ -0,0 +1,13 @@
+var colorSelected;
+function colorValue(color){
+	colorSelected=color;
+}
+
+function colorMe(number){
+	document.getElementById(number).style.backgroundColor=colorSelected;
+}
+
+function refresh(){
+	window.location.reload();
+}
+
