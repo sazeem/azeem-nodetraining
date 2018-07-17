80dee24e18aaae22883ec5529eb22e88cf0edb50
Syed Azeem <f2014185@goa.bits-pilani.ac.in>
2018-07-17 10:28:07 +0530

    Adding assignment-1 on Node

---
+++ Module-2 | Day-2/CalculateFrequency.js
@@ -0,0 +1,21 @@
+﻿function calculateFrequency(string) {
+    var stringArr = string.split("");
+    function charFreq(char){
+        var count = 0;
+        for (var i=0; i<stringArr.length; i++){
+            if (char==stringArr[i]){
+                count+=1;
+            }
+        }
+        return count;
+    }
+    var set=new Set(stringArr);
+    var newStringArr=[...set];
+    var output=new Object();
+    
+    for (var i=0;i<newStringArr.length;i++){
+        var char=newStringArr[i];
+        output[char]=charFreq(char);
+    }
+    return output;
+}
---
+++ Module-2 | Day-2/SecondLargest.js
@@ -0,0 +1,22 @@
+﻿function secondLargest(array) {
+  var lar=array[0],secLar;
+
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > lar) {
+      lar = array[i];
+    }
+  }
+    
+  if(array[0]==lar)
+      secLar=array[1];
+  else
+      secLar=array[0];
+    
+  for (var j = 0; j < array.length; j++) {
+    if (array[j] >= secLar && array[j] < lar && secLar != lar) {
+      secLar = array[j];
+    }
+  }
+
+  return secLar;
+}
