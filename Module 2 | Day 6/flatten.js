34db9265fb9c4ce8882b5f17e0cf53eb7f744080
Syed <sazeem@grepruby.com>
2018-07-20 21:38:14 +0530

    Adding folder on Unit testing

---
+++ Module 2 | Day 6/flatten.js
@@ -0,0 +1,23 @@
+﻿const flatten = function(unflatObject) {     
+  var output={};            
+  var rec_key='';
+
+  function flatter(unflatObject,rec_key){
+    
+    for(let key in unflatObject){                     
+      if(typeof(unflatObject[key])!='object'){
+          output[rec_key+key]=unflatObject[key];
+      }
+      else{                 
+        var temp_key = rec_key;
+        rec_key = rec_key + key + ".";          
+        Object.assign(output,flatter(unflatObject[key],rec_key));             
+        rec_key = temp_key;
+      }
+    }
+  }
+  flatter(unflatObject,rec_key);              
+  return output;             
+}
+
+module.exports = {flatten};
