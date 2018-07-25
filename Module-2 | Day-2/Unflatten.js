bb60b57ae6f32b75a8b565b3a2b481e4340eb3d0
Syed <sazeem@grepruby.com>
2018-07-25 12:42:58 +0530

    Unflatten.js

--- Module-2 | Day-2/Unflatten.js
+++ Module-2 | Day-2/Unflatten.js
@@ -1,5 +1,15 @@
-﻿function Unflatten(flatObject){
-  
+﻿const input = { "flatJSON": false, "i.am.not.so.flat": true, "i.am.not.so.unflat": false, "i.am.a": "tree", "dates.0.day": 1, "dates.1.day": 8947 };
+//output = { "flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] };
+
+function Unflatten(flatObject){
+	const myarray = Object.keys(flatObject);
+  var mykeys = [];
+  for(var index = 0; index < myarray.length; index++){
+    mykeys.push(myarray[index].split("."));
+  }
+  console.log(mykeys);
 }
 
-module.exports = {Unflatten};
+
+Unflatten(input);
+// module.exports = {Unflatten};
