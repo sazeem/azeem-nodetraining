f341d510a6700e685df0a2287845316bd1d3577e
Syed <sazeem@grepruby.com>
2018-08-06 15:16:07 +0530

    changes

--- Module-1 | Day-3/DOMScript.js
+++ Module-1 | Day-3/DOMScript.js
@@ -1,20 +1,20 @@
-var colorSelected;
-var colorRetain;
+let colorSelected;
+let colorRetain;
 
-function colorValue(color){
-  colorSelected=color;
-}
+const colorValue = (color) => {
+  colorSelected = color;
+};
 
-function colorMe(number){
-  document.getElementById(number).style.backgroundColor=colorSelected;
-}
+const colorMe = (number) => {
+  document.getElementById(number).style.backgroundColor = colorSelected;
+};
 
-function refresh(){
+const refresh = () => {
   colorRetain=colorSelected;
   colorSelected='gray';
   for(var index=1;index<10;index++){
     document.getElementById(index).style.backgroundColor=colorSelected;
   }
   colorSelected=colorRetain;
-}
+};
 
