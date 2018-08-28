03a6766172054b1f5cc473f7ae9b478e2eb4088d
Syed <sazeem@grepruby.com>
2018-08-28 17:24:28 +0530

    changes

--- Module-3 | Projects with GitHub/routes/index.js
+++ Module-3 | Projects with GitHub/routes/index.js
@@ -6,15 +6,15 @@ const contributorRoutes = require('./contributorRoutes');
 const pullRequestRoutes = require('./pullRequestRoutes');
 
 const routes = (app) => {
-	app.use(bodyParser.urlencoded({ 
-		extended: true 
-	}));
-	app.use(bodyParser.json());
   app.use(`/`, projectRoutes());
   app.use(`/`, repoRoutes());
   app.use(`/`, commitRoutes());
   app.use(`/`, contributorRoutes());
   app.use(`/`, pullRequestRoutes());
+	app.use(bodyParser.urlencoded({ 
+		extended: true 
+	}));
+	app.use(bodyParser.json());
 }
 
 module.exports = routes;
