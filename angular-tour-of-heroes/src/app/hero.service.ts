0a7a4cf6f4b9999c9a5aa27a240ef41c413207a3
Syed <sazeem@grepruby.com>
2018-08-31 15:21:04 +0530

    added dummy-angular-project

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -69,4 +69,15 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+  
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
