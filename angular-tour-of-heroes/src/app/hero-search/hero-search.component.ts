bee5e96fb46f7669b7107698fd2e70c3b2046d84
Syed <sazeem@grepruby.com>
2018-08-31 15:57:01 +0530

    added dummy-angular-project

--- angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -26,15 +26,10 @@ export class HeroSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
+    this.heroes$ = this.searchTerms.pipe(      
       debounceTime(300),
-
-      // ignore new term if same as previous term
       distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
       switchMap((term: string) => this.heroService.searchHeroes(term)),
     );
   }
-}
+} 
